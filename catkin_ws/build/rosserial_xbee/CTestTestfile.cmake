6f6a6dcb688e3a470452b6c8e7f04590c3ac1211
Victor T. Noppeney <victor.noppeney@usp.br>
2016-09-10 09:18:56 -0300

    Added XBee and .gitignore

--- catkin_ws/build/rosserial_xbee/CTestTestfile.cmake
+++ catkin_ws/build/rosserial_xbee/CTestTestfile.cmake
@@ -1,8 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/victor/labrom/catkin_ws/src
-# Build directory: /home/victor/labrom/catkin_ws/build
+# Source directory: /home/victor/labrom/catkin_ws/src/rosserial_xbee
+# Build directory: /home/victor/labrom/catkin_ws/build/rosserial_xbee
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(rosserial_xbee)
